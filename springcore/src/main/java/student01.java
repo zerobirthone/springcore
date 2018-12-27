b92c1ef15e461109610902472807382934221a14
gitroot <zerobirthone@163.com>
2018-12-27 20:23:07 +0800

    gitspringcorej+++subersion

--- springcore/src/main/java/student01.java
+++ springcore/src/main/java/student01.java
@@ -2,4 +2,8 @@ public class student01 {
     public static void main(String[] args) {
         System.out.println("firstkiss");
     }
+    public void subVersion(){
+        System.out.println("subversion");
+    }
+
 }
